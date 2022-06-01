1bc6e00a120281003b1a4aef4b5ae71e2c4666db
LordKillZone <p4513632@gmail.com>
2022-06-01 14:54:59 +0200

    update

--- main_stl.py
+++ main_stl.py
@@ -7,21 +7,21 @@ import mpl_toolkits
 from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
-from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
 Tk().withdraw() 
-filename = askopenfilename()
+filename = askopenfilename()            #wybierniepliku .stl do wgrani do wyswietlacza
 
 
 
 figure = pyplot.figure()
 axes = mplot3d.Axes3D(figure)
 
-your_mesh = mesh.Mesh.from_file(filename)
+your_mesh = mesh.Mesh.from_file(filename)       #Otwarcie wybranego pliku .stl
 axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
 
 scale = your_mesh.points.flatten()
-axes.auto_scale_xyz(scale, scale, scale)
+axes.auto_scale_xyz(scale, scale, scale)        #automatyczne przeskalowanie pliku
 
-pyplot.show()
+pyplot.show()       #otwarcie pliku w GUI podobny do MATLABA
