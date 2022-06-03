6ed3e470b06ed06a55eacd7c77e55005540ff08e
LordKillZone <p4513632@gmail.com>
2022-06-03 08:21:02 +0200

    Dodanie funkcji
    Dodalem okno z informacjami o autorze, przyciski, wychodzenie

--- main_fe.py
+++ main_fe.py
@@ -3,11 +3,32 @@
 
 import tkinter as tk
 from tkinter import ttk
+import sys
+
 
 window = tk.Tk()
 window.title('TotalSTL')
 window.geometry('1000x800+50+50')
 window.iconbitmap('icon.ico')
-ttk.Label(window, text="Testowanie tego mordo").pack()
+
+
+
+def button_clicked():
+    okno =tk.Tk()
+    def exit():
+        okno.destroy()
+    okno.title('Information')
+    okno.geometry('800x100+5+5')
+    ttk.Label(okno, text="TotalSTL").pack()
+    ttk.Label(okno, text="version: 0.0.1").pack()
+    ttk.Label(okno, text="Author: Piotr Pawlik").pack()
+    ttk.Label(okno, text="Jest to oprogramowanie pozwalajace na wyswietlanie plikow .STL, zarządzanie nimi i przeglądanie plikow .GCODE dla drukarek 3D").pack()
+    ttk.Button(okno, text="EXIT", command=exit).pack()
+
+
+        
+ttk.Button(window, text="TotalSTL ver.0.0.1", command=button_clicked).pack()
+ttk.Button(window, text="EXIT", command=exit).pack()
+
 
 window.mainloop()
